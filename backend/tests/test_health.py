59da22bd7a1932d02ebe8976438b28111255b474
caio-stat <caiocostacavalcante6@gmail.com>
2026-06-10 00:53:43 -0300

    test: add API endpoint tests

---
+++ backend/tests/test_health.py
@@ -0,0 +1,20 @@
+from fastapi.testclient import TestClient
+
+from app.main import app
+
+client = TestClient(app)
+
+def test_root_endpoint():
+    response = client.get("/health")
+
+    assert response.status_code == 200
+    assert response.json() == {"status": "ok"}
+
+def test_version_endpoint():
+    response = client.get("/version")
+
+    assert response.status_code == 200
+    assert response.json() == {
+        "name": "Atlas API",
+        "version" : "0.1.0"
+    } 
